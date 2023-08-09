a19434795b2bc9f932bd337ab6785c6a7efafeec
haany <hany.cher@gmail.com>
2023-08-09 17:37:53 +0100

    Features: Add functionalities the the UI

--- EggTimer/Controller/ViewController.swift
+++ EggTimer/Controller/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -30,8 +30,9 @@ class ViewController: UIViewController {
 
   }
 
-  let selectedEggTitle: UILabel = UILabel()
+ //MARK: - UIKit elements
 
+  let selectedEggTitle: UILabel = UILabel()
   let eggsStackView: UIStackView = UIStackView()
   let firstLineStackView: UIStackView = UIStackView()
   let secondLineStackView: UIStackView = UIStackView()
@@ -40,9 +41,13 @@ class ViewController: UIViewController {
   let mediumEggButton: UIButton = UIButton()
   let hardEggButton: UIButton = UIButton()
   let overEggButton: UIButton = UIButton()
+  var allEggs = [UIButton]()
 
-
-  var selectedEgg: String = Constants.defaultTitle
+  let timeProgressBar: UIProgressView = UIProgressView(progressViewStyle: .bar)
+  var timer: Timer = Timer()
+  var selectedEgg: Egg = Egg(title: Constants.defaultTitle, time: 100)
+  var currentProgress: Double = 0
+  var remainingTime: Double = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -50,8 +55,58 @@ class ViewController: UIViewController {
     layout()
   }
 
+  @objc func buttonSelected(_ sender: UIButton!) {
+
+    switch sender {
+      case softEggButton:
+        selectedEgg = Constants.softEggTitle
+      case mediumEggButton:
+        selectedEgg = Constants.mediumEggTitle
+      case hardEggButton:
+        selectedEgg = Constants.hardEggTitle
+      case overEggButton:
+        selectedEgg = Constants.overEggTitle
+      default:
+        print("Error: wrong button")
+    }
+
+    timeProgressBar.isHidden = false
+    timer.invalidate()
+    timeProgressBar.progress = 0
+    currentProgress = 0
+    focusOnTheSelectedEgg(sender)
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timesUp), userInfo: nil, repeats: true)
+  }
+
+  @objc func timesUp() {
+
+    if currentProgress != selectedEgg.time {
+      currentProgress += 1
+      remainingTime = selectedEgg.time - currentProgress
+      timeProgressBar.setProgress(Float( currentProgress / selectedEgg.time), animated: true)
+      selectedEggTitle.text = "\(selectedEgg.title)\n will be ready in \(Int(remainingTime))s"
+
+    }else{
+      currentProgress = 0
+      timer.invalidate()
+      timeProgressBar.isHidden = true
+      selectedEggTitle.text = "\(selectedEgg.title) is ready"
+    }
+  }
+
+  func focusOnTheSelectedEgg(_ sender: UIButton!) {
+    for egg in allEggs {
+      if sender != egg {
+        egg.layer.opacity = 0.5
+        egg.sizeThatFits(CGSize(width: 30, height: 30))
+      } else {
+        egg.layer.opacity = 1
+      }
+    }
+  }
 
   }
+
 
 //MARK: - Private
   private func setup() {
@@ -61,9 +116,11 @@ class ViewController: UIViewController {
     view.addSubview(eggsStackView)
     view.addSubview(timeProgressBar)
 
-    selectedEggTitle.text = selectedEgg
+    selectedEggTitle.text = selectedEgg.title
     selectedEggTitle.textColor = .darkText
-    selectedEggTitle.font = .systemFont(ofSize: 30,weight: .bold)
+    selectedEggTitle.numberOfLines = 2
+    selectedEggTitle.textAlignment = .center
+    selectedEggTitle.font = .systemFont(ofSize: 30,weight: .medium)
     selectedEggTitle.translatesAutoresizingMaskIntoConstraints = false
 
     eggsStackView.initStackView(axis: .vertical)
@@ -76,12 +133,22 @@ class ViewController: UIViewController {
     setupHorizontalStackView(secondLineStackView, arrangedSubViews: [hardEggButton, overEggButton])
 
 
+    softEggButton.initUIButton(self, named:"soft_egg", action: #selector(buttonSelected))
+    mediumEggButton.initUIButton(self, named: "medium_egg", action: #selector(buttonSelected))
+    hardEggButton.initUIButton(self, named: "hard_egg", action: #selector(buttonSelected))
+    overEggButton.initUIButton(self, named: "over_egg", action: #selector(buttonSelected))
+
+    allEggs.append(softEggButton)
+    allEggs.append(mediumEggButton)
+    allEggs.append(hardEggButton)
+    allEggs.append(overEggButton)
 
     timeProgressBar.translatesAutoresizingMaskIntoConstraints = false
-    timeProgressBar.setProgress(0.5, animated: false)
+    timeProgressBar.setProgress(0, animated: true)
     timeProgressBar.trackTintColor = .systemGray6
     timeProgressBar.tintColor = .systemYellow
     timeProgressBar.transform = CGAffineTransformMakeScale(1, 4)
+    timeProgressBar.isHidden = true
 
   }
 
@@ -105,6 +172,7 @@ class ViewController: UIViewController {
 
       selectedEggTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
       selectedEggTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      selectedEggTitle.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
 
       timeProgressBar.topAnchor.constraint(equalTo: eggsStackView.bottomAnchor, constant: 100),
       timeProgressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
--- EggTimer/Model/Egg.swift
+++ EggTimer/Model/Egg.swift
@@ -10,7 +10,10 @@ import Foundation
 struct Egg {
   let title: String
   let time: Double
-
+  init(title: String, time: Double) {
+    self.title = title
+    self.time = time * 60
+  }
   func calculateTime() -> Double {
     return time * 60
   }
